b6f4794c4cdb5c55177afc79383c027e0e7416e5
Wong Kok Ian <ianwong97@hotmail.sg>
2020-05-17 10:31:29 +0800

    Added tempData and components

---
+++ tempData.js
@@ -0,0 +1,38 @@
+export default tempData = [
+    {
+        name: 'Plan a Trip',
+        color: '#24A6D9',
+        todos: [
+            {
+                title: 'Book Flight',
+                completed: false,
+            },
+            {
+                title: 'Check Passport',
+                completed: true,
+            },
+            {
+                title: 'Reserve Hotel',
+                completed: true,
+            },
+            {
+                title: 'Pack Luggage',
+                completed: false,
+            }
+        ]
+    },
+    {
+        name: 'Errands',
+        color: '#8022D9',
+        todos: [
+            {
+                title: 'Buy Milk',
+                completed: false,
+            },
+            {
+                title: 'Buy Eggs',
+                completed: true,
+            },
+        ]
+    },
+];
