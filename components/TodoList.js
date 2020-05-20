0fa973e62478b746915dce0d23a23baa91e6a506
yeoutzer <loo_yeou_tzer@hotmail.com>
2020-05-20 20:11:46 +0800

    Added the addition of subtasks.

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,38 +1,64 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import colors from '../Colors';
+import { StyleSheet, Text, View, TouchableOpacity, Modal } from 'react-native';
+import TodoModal from "./TodoModal"
 
-export default TodoList = ({list}) => {
-    const completedCount = list.todos.filter(todo => todo.completed).length;
-    const remainingCount = list.todos.length - completedCount;
-    return (
-        <View style={[styles.listContainer, {borderColor: list.color}]}>
-            <Text style={[styles.listTitle], {color: list.color}} numberOfLines={1}>
-                {list.name}
-            </Text>
+export default class TodoList extends React.Component {
+    state = {
+        showListVisible: false
+    };
+
+    toggleListModal() {
+        this.setState({ showListVisible: !this.state.showListVisible });
+    }
+
+    render() {
+        const list = this.props.list;
+        const completedCount = list.todos.filter(todo => todo.completed).length;
+        const remainingCount = list.todos.length - completedCount;
+
+        return (
             <View>
-                <View style={{alignItems: 'center'}}>
-                    <Text style={[styles.count, {color: list.color}]}>{remainingCount}</Text>
-                    <Text style={[styles.subtitle, {color: list.color}]}>Remaining</Text>
-                </View>
-                <View style={{alignItems: 'center'}}>
-                    <Text style={[styles.count, {color: list.color}]}>{completedCount}</Text>
-                    <Text style={[styles.subtitle, {color: list.color}]}>Completed</Text>
-                </View>
-                <View style={{alignItems: 'center'}}>
-                    {list.remind == true
-                        ? <Text style={[styles.count, {color: list.color}]}>
-                            {Math.floor((list.date.getTime() - new Date().getTime()) / (1000 * 60 * 60 * 24))}
-                        </Text>
-                        : null}
-                    {list.remind == true
-                        ? <Text style={[styles.subtitle, {color: list.color}]}>Countdown</Text>
-                        : null}
-                </View>
+                <Modal
+                    animationType="slide"
+                    visible={this.state.showListVisible}
+                    onRequestClose={() => this.toggleListModal()}
+                >
+                    <TodoModal list = {list} closeModal = {() => this.toggleListModal()} />
+                </Modal>
 
+                <TouchableOpacity
+                    style={[styles.listContainer, { borderColor: list.color }]}
+                    onPress={() => this.toggleListModal()}
+                >
+                    <Text style={[styles.listTitle], { color: list.color }} numberOfLines={1}>
+                        {list.name}
+                    </Text>
+
+                    <View>
+                        <View style={{ alignItems: 'center' }}>
+                            <Text style={[styles.count, { color: list.color }]}>{remainingCount}</Text>
+                            <Text style={[styles.subtitle, { color: list.color }]}>Remaining</Text>
+                        </View>
+                        <View style={{ alignItems: 'center' }}>
+                            <Text style={[styles.count, { color: list.color }]}>{completedCount}</Text>
+                            <Text style={[styles.subtitle, { color: list.color }]}>Completed</Text>
+                        </View>
+                        <View style={{ alignItems: 'center' }}>
+                            {list.remind == true
+                                ? <Text style={[styles.count, { color: list.color }]}>
+                                    {Math.floor((list.date.getTime() - new Date().getTime()) / (1000 * 60 * 60 * 24))}
+                                </Text>
+                                : null}
+                            {list.remind == true
+                                ? <Text style={[styles.subtitle, { color: list.color }]}>Countdown</Text>
+                                : null}
+                        </View>
+
+                    </View>
+                </TouchableOpacity>
             </View>
-        </View>
-    );
+        );
+    }
 };
 
 const styles = StyleSheet.create({
