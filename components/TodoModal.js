c338ef2115732cf9dce7c7ccb83c2e7fae6cd7f5
yeoutzer <loo_yeou_tzer@hotmail.com>
2020-05-21 20:12:46 +0800

    Synced with original file from master.

--- components/TodoModal.js
+++ components/TodoModal.js
@@ -1,33 +1,60 @@
-import React from "react";
-import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, FlatList, KeyboardAvoidingView, TextInput } from "react-native";
-import { AntDesign, Ionicons } from '@expo/vector-icons';
-import colors from "../Colors";
+import React from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    TouchableOpacity,
+    FlatList,
+    KeyboardAvoidingView,
+    TextInput,
+    Keyboard
+} from 'react-native';
+import {AntDesign, Ionicons} from '@expo/vector-icons';
+import colors from '../Colors';
 
 export default class TodoModal extends React.Component {
     state = {
-        name: this.props.list.name,
-        color: this.props.list.color,
-        remind: this.props.list.remind,
-        date: this.props.list.date,
-        todos: this.props.list.todos
+        newTodo: ''
+    };
+
+    toggleTodoCompleted = index => {
+        let list = this.props.list
+        list.todos[index].completed = !list.todos[index].completed
+
+        this.props.updateList(list);
     }
 
-    renderTodo = todo => {
+    addTodo = () => {
+      let list = this.props.list;
+      list.todos.push({title: this.state.newTodo, completed: false});
+
+      this.props.updateList(list);
+      this.setState({newTodo: ''});
+
+      Keyboard.dismiss();
+    };
+
+    renderTodo = (todo, index) => {
         return (
             <View style={styles.todoContainer}>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => this.toggleTodoCompleted(index)}>
                     <Ionicons
-                        name={todo.completed ? "ios-square" : "ios-square-outline"}
+                        name={todo.completed ? 'ios-square' : 'ios-square-outline'}
                         size={24}
                         color={colors.gray}
-                        style={{ width: 32 }}
+                        style={{width: 32}}
                     />
                 </TouchableOpacity>
-                <Text style={[styles.todo,
-                {
-                    textDecorationLine: todo.completed ? "line-through" : "none",
-                    color: todo.completed ? colors.gray : colors.black
-                }]}
+
+                <Text
+                    style={[
+                        styles.todo,
+                        {
+                            textDecorationLine: todo.completed ? 'line-through' : 'none',
+                            color: todo.completed ? colors.gray : this.props.list.color
+                        }
+                    ]}
                 >
                     {todo.title}
                 </Text>
@@ -36,81 +63,89 @@ export default class TodoModal extends React.Component {
     };
 
     render() {
-        const taskCount = this.state.todos.length
-        const completedCount = this.state.todos.filter(todo => todo.completed).length
+        const list = this.props.list
+
+        const taskCount = list.todos.length
+        const completedCount = list.todos.filter(todo => todo.completed).length
 
         return (
+            <KeyboardAvoidingView style={{flex: 1}} behavior='padding'>
             <SafeAreaView style={styles.container}>
                 <TouchableOpacity
-                    style={{ position: 'absolute', top: 64, right: 32, zIndex: 10 }}
+                    style={{position: 'absolute', top: 64, right: 32, zIndex: 10}}
                     onPress={this.props.closeModal}
                 >
-                    <AntDesign name="close" size={24} color={colors.black} />
+                    <AntDesign name='close' size={24} color={colors.white}/>
                 </TouchableOpacity>
-
-                <View style={[styles.section, styles.header, { borderBottomColor: this.state.color }]}>
+                <View style={[styles.section, styles.header, {borderBottomColor: list.color}]}>
                     <View>
-                        <Text style={styles.title}>{this.state.name}</Text>
+                        <Text style={styles.title}>{list.name}</Text>
                         <Text style={styles.taskCount}>
                             {completedCount} of {taskCount} tasks
                         </Text>
                     </View>
                 </View>
-
-                <View style={[styles.section, { flex: 3 }]}>
+                <View style={[styles.section, {flex: 3}]}>
                     <FlatList
-                        data={this.state.todos}
-                        renderItem={({ item }) => this.renderTodo(item)}
+                        data={list.todos}
+                        renderItem={({item, index}) => this.renderTodo(item, index)}
                         keyExtractor={item => item.title}
-                        contentContainerStyle={{ paddingHorizontal: 32, paddingVertical: 64 }}
-                        showsVerticalScrollIndicator={false}
+                        contentContainerStyle={{paddingHorizontal: 32, paddingVertical: 64}}
+                        showVerticalScrollIndicator={false}
                     />
                 </View>
 
-                <KeyboardAvoidingView styles={[styles.section, styles.footer]} behavior="padding">
-                    <TextInput style={[styles.input, { borderColor: this.state.color }]} />
-                    <TouchableOpacity style={[styles.addTodo, { backgroundColor: this.state.color }]}>
-                        <AntDesign name="plus" size={16} color={colors.white} />
+                <View style={[styles.section, styles.footer]}>
+                    <TextInput
+                        style={[styles.input, {borderColor: list.color, color: colors.white}]}
+                        onChangeText={text => this.setState({newTodo: text})}
+                        value={this.state.newTodo}
+                    />
+                    <TouchableOpacity
+                        style={[styles.addTodo, {backgroundColor: list.color}]}
+                        onPress={() => this.addTodo()}
+                    >
+                        <AntDesign name='plus' size={16} color={colors.white}/>
                     </TouchableOpacity>
-                </KeyboardAvoidingView>
+                </View>
             </SafeAreaView>
-
+            </KeyboardAvoidingView>
         );
-
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: colors.white,
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#1A1A1A',
     },
     section: {
         flex: 1,
-        alignSelf: "stretch"
+        alignSelf: 'stretch',
     },
     header: {
-        justifyContent: "flex-end",
+        justifyContent: 'flex-end',
         marginLeft: 64,
-        borderBottomWidth: 3
+        borderBottomWidth: 3,
     },
     title: {
         fontSize: 30,
-        fontWeight: "800",
-        color: colors.black
+        fontWeight: '800',
+        color: colors.white,
     },
     taskCount: {
         marginTop: 4,
         marginBottom: 16,
         color: colors.gray,
-        fontWeight: "600"
+        fontWeight: '600',
+        opacity: 0.5,
     },
     footer: {
         paddingHorizontal: 32,
-        flexDirection: "row",
-        alignItems: "center"
+        flexDirection: 'row',
+        alignItems: 'center',
     },
     input: {
         flex: 1,
@@ -118,22 +153,22 @@ const styles = StyleSheet.create({
         borderWidth: StyleSheet.hairlineWidth,
         borderRadius: 6,
         marginRight: 8,
-        paddingHorizontal: 8
+        paddingHorizontal: 8,
     },
     addTodo: {
         borderRadius: 4,
         padding: 16,
-        alignItems: "center",
-        justifyContent: "center"
+        alignItems: 'center',
+        justifyContent: 'center',
     },
     todoContainer: {
         paddingVertical: 16,
-        flexDirection: "row",
-        alignItems: "center"
+        flexDirection: 'row',
+        alignItems: 'center',
     },
     todo: {
         color: colors.black,
-        fontWeight: "700",
-        fontSize: 16
+        fontWeight: '700',
+        fontSize: 16,
     }
-})
+});
